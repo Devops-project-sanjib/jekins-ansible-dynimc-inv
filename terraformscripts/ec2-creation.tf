b8b9277b91f48718b6a6ceff42ee927c5f4fa089
rupsan1234 <63985986+rupsan1234@users.noreply.github.com>
2024-07-29 11:03:41 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,10 +4,10 @@ region = "ap-south-1"
 
 resource "aws_instance" "AWS_EC2INSTANCE"{
 ami = "ami-068e0f1a600cd311c"
-instance_type = "t2.micro"
-key_name = "devopsmssjuly"
-security_groups = ["launch-wizard-2"]
-tags = {
+instance_type= "t2.micro"
+key_name= "Ansible-key"
+security_groups= ["launch-wizard-5"]
+tags= {
 Name = "terraformserver"
 }
 }
